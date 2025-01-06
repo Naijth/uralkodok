27170c711d8b6732f9f68cbc58182295e8325f96
Naijth <163039373+Naijth@users.noreply.github.com>
2025-01-06 17:23:24 +0100

    egyszeru validacio fuggvenybe szervezese, parameterek bevezetese
    HOI MATE. I WANNA KILL MESELF

--- main.js
+++ main.js
@@ -44,36 +44,36 @@ const form = document.getElementById('form'); // this puts the thing that has th
 const table = document.createElement('table'); // creates the table in the const table variable
 document.body.appendChild(table); // appends said table to the body
 
-// define the table structure with a colgroup
-const colgroup = document.createElement('colgroup'); // creates a colgroup in the const colgroup variable
-table.appendChild(colgroup); // appends colgroup to the previously created table
-const col1 = document.createElement('col'); // creates a column in the const col1 variable
-colgroup.appendChild(col1); // appends col1 to colgroup
-col1.className = "column"; // gives col1 the "column" class
-const col2 = document.createElement('col'); // creates a column in the const col2 variable
-colgroup.appendChild(col2); // appends col2 to colgroup
-const col3 = document.createElement('col'); // creates a column in the const col3 variable
-colgroup.appendChild(col3); // appends col3 to colgroup
-col3.className = "column"; // gives col3 the "column" class
-
-// create the table header
-const thead = document.createElement('thead'); // creates the table header element in the const thead variable
-table.appendChild(thead); // appends thead to the previously created table
-const tr1 = document.createElement('tr'); // creates a row in the const tr1 variable for the header
-thead.appendChild(tr1); // appends tr1 to thead
-const th1 = document.createElement('th'); // creates a table header cell in the const th1 variable
-tr1.appendChild(th1); // appends th1 to tr1
-th1.innerHTML = array[0].uralkodo; // sets the text of th1 to the variable uralkodo's content
-const th2 = document.createElement('th'); // creates a table header cell in the const th2 variable
-tr1.appendChild(th2); // appends th2 to tr1
-th2.innerHTML = array[0].esemeny; // sets the text of th2 to the variable esemeny's content
-const th3 = document.createElement('th'); // creates a table header cell in the const th3 variable
-tr1.appendChild(th3); // appends th3 to tr1
-th3.innerHTML = array[0].evszam; // sets the text of th3 to the variable evszam's content
-const tbody = document.createElement('tbody'); // creates the table body in the const tbody variable
-table.appendChild(tbody); // appends tbody to the table
-
 function renderMenu(){ //this is the function
+    // define the table structure with a colgroup
+    const colgroup = document.createElement('colgroup'); // creates a colgroup in the const colgroup variable
+    table.appendChild(colgroup); // appends colgroup to the previously created table
+    const col1 = document.createElement('col'); // creates a column in the const col1 variable
+    colgroup.appendChild(col1); // appends col1 to colgroup
+    col1.className = "column"; // gives col1 the "column" class
+    const col2 = document.createElement('col'); // creates a column in the const col2 variable
+    colgroup.appendChild(col2); // appends col2 to colgroup
+    const col3 = document.createElement('col'); // creates a column in the const col3 variable
+    colgroup.appendChild(col3); // appends col3 to colgroup
+    col3.className = "column"; // gives col3 the "column" class
+
+    // create the table header
+    const thead = document.createElement('thead'); // creates the table header element in the const thead variable
+    table.appendChild(thead); // appends thead to the previously created table
+    const tr1 = document.createElement('tr'); // creates a row in the const tr1 variable for the header
+    thead.appendChild(tr1); // appends tr1 to thead
+    const th1 = document.createElement('th'); // creates a table header cell in the const th1 variable
+    tr1.appendChild(th1); // appends th1 to tr1
+    th1.innerHTML = array[0].uralkodo; // sets the text of th1 to the variable uralkodo's content
+    const th2 = document.createElement('th'); // creates a table header cell in the const th2 variable
+    tr1.appendChild(th2); // appends th2 to tr1
+    th2.innerHTML = array[0].esemeny; // sets the text of th2 to the variable esemeny's content
+    const th3 = document.createElement('th'); // creates a table header cell in the const th3 variable
+    tr1.appendChild(th3); // appends th3 to tr1
+    th3.innerHTML = array[0].evszam; // sets the text of th3 to the variable evszam's content
+    const tbody = document.createElement('tbody'); // creates the table body in the const tbody variable
+    table.appendChild(tbody); // appends tbody to the table
+
     for (let i = 1; i < array.length; i += 2){ // the cycle goes through the array 2 at a time
         const tableLine = document.createElement('tr'); // creates a variable named tableLine
         tbody.appendChild(tableLine); // appends tableLine to tbody
@@ -103,7 +103,38 @@ function renderMenu(){ //this is the function
 };
 renderMenu(); //this calls said function
 
-form.addEventListener('submit', function(e){
+function validateField(inputElement){ // creates the function
+    let valid = true; // creates this boolean
+    if (inputElement.value == ''){ // checks if the variable is empty
+        const parent = inputElement.parentElement; // puts the parent element into the variable
+        const error = parent.querySelector('.error'); // we find the first thing with the error group and put it in the variable
+        if (error != undefined){ // if said div is empty
+            error.innerHTML = 'Ez a mező kötelező!'; // we put this text in it
+        }
+        valid = false; // sets the boolean to false
+    }
+    return valid; // returns this boolean
+}
+
+function validateFieldEitherOr(inputElement1, inputElement2) { // creates the function
+    let valid = true; // creates this boolean
+    if (inputElement1.value != '' && inputElement2.value == '' || inputElement1.value == '' && inputElement2.value != ''){ // checks if either one is empty but the other isnt
+        const parent1 = inputElement1.parentElement; // puts the parent element into the variable
+        const parent2 = inputElement2.parentElement; // puts the parent element into the variable
+        const error1 = parent1.querySelector('.error'); // we find the first thing with the error group and put it in the variable
+        const error2 = parent2.querySelector('.error'); // we find the first thing with the error group and put it in the variable
+        if (error1 != undefined){ // if said div is empty
+            error1.innerHTML = 'Mind két mező kötelező, ha 2 sort akar!'; // we put this text in it
+        }
+        if (error2 != undefined){ // if said div is empty
+            error2.innerHTML = 'Mind két mező kötelező, ha 2 sort akar!'; // we put this text in it
+        }
+        valid = false; // sets the boolean to false
+    }
+    return valid; // returns this boolean
+}
+
+form.addEventListener('submit', function(e){ // checks if anyone pressed the funny button
     e.preventDefault(); // this prevents the site from running it's default function upon pressing the shiny button
     const urNevElement = document.getElementById('uralkodo_nev') // this puts the htmlElement with the corresponding id into this variable
     const esemeny1Element = document.getElementById('esemeny1') // this puts the htmlElement with the corresponding id into this variable
@@ -119,66 +150,41 @@ form.addEventListener('submit', function(e){
 
     let valid = true; // this creates the valid boolean
 
-    const urNevValue = urNevElement.value; // this puts the value of the element into this variable
-    const esemeny1Value = esemeny1Element.value; // this puts the value of the element into this variable
-    const evszam1Value = evszam1Element.value; // this puts the value of the element into this variable
-    const esemeny2Value = esemeny2Element.value == "" ? undefined : esemeny2Element.value; // this puts the value of the element into this variable, unless it is empty, in which case it becomes undefined
-    const evszam2Value = evszam2Element.value == "" ? undefined : evszam2Element.value; // this puts the value of the element into this variable, unless it is empty, in which case it becomes undefined
+    if (!validateField(urNevElement)){ // runs this function with the element in it
+        valid = false; // sets the boolean to false
+    }
+    if (!validateField(esemeny1Element)){ // runs this function with the element in it
+        valid = false; // sets the boolean to false
+    }
+    if (!validateField(evszam1Element)){ // runs this function with the element in it
+        valid = false; // sets the boolean to false
+    }
+    if (!validateFieldEitherOr(esemeny2Element, evszam2Element)){ // runs this function with the elements in it
+        valid = false; // sets the boolean to false
+    }
 
-    if (urNevValue == ''){ // checks if the variable is empty
-        const parent = urNevElement.parentElement; // puts the parent element into the variable
-        const error = parent.querySelector('.error'); // we find the first thing with the error group and put it in the variable
-        if (error != undefined){ // if said div is empty
-            error.innerHTML = 'Ez a mező kötelező!'; // we put this text in it
-        }
-        valid = false; // sets the boolean to false
-    }
-    if (esemeny1Value == ''){ // checks if the variable is empty
-        const parent = esemeny1Element.parentElement; // puts the parent element into the variable
-        const error = parent.querySelector('.error'); // we find the first thing with the error group and put it in the variable
-        if (error != undefined){ // if said div is empty
-            error.innerHTML = 'Ez a mező kötelező!'; // we put this text in it
-        }
-        valid = false; // sets the boolean to false
-    }
-    if (evszam1Value == ''){ // checks if the variable is empty
-        const parent = evszam1Element.parentElement; // puts the parent element into the variable
-        const error = parent.querySelector('.error'); // we find the first thing with the error group and put it in the variable
-        if (error != undefined){ // if said div is empty
-            error.innerHTML = 'Ez a mező kötelező!'; // we put this text in it
-        }
-        valid = false; // sets the boolean to false
-    }
-    if (esemeny2Value != undefined && evszam2Value == undefined || esemeny2Value == undefined && evszam2Value != undefined){
-        const parentEsemeny = esemeny2Element.parentElement; // puts the parent element into the variable
-        const parentEvszam = evszam2Element.parentElement; // puts the parent element into the variable
-        const errorEsemeny = parentEsemeny.querySelector('.error'); // we find the first thing with the error group and put it in the variable
-        const errorEvszam = parentEvszam.querySelector('.error'); // we find the first thing with the error group and put it in the variable
-        if (errorEsemeny != undefined){ // if said div is empty
-            errorEsemeny.innerHTML = 'Mind két mező kötelező, ha 2 sort akar!'; // we put this text in it
-        }
-        if (errorEvszam != undefined){ // if said div is empty
-            errorEvszam.innerHTML = 'Mind két mező kötelező, ha 2 sort akar!'; // we put this text in it
-        }
-        valid = false; // sets the boolean to false
-    }
-    if (valid){
+    if (valid){ //checks if the boolean is true
+        const urNevValue = urNevElement.value; // this puts the value of the element into this variable
+        const esemeny1Value = esemeny1Element.value; // this puts the value of the element into this variable
+        const evszam1Value = evszam1Element.value; // this puts the value of the element into this variable
+        const esemeny2Value = esemeny2Element.value == "" ? undefined : esemeny2Element.value; // this puts the value of the element into this variable, unless it is empty, in which case it becomes undefined
+        const evszam2Value = evszam2Element.value == "" ? undefined : evszam2Element.value; // this puts the value of the element into this variable, unless it is empty, in which case it becomes undefined
+        
         const newElement = { // we make new element
             uralkodo: urNevValue, // the new element's value here becometh the called variable
             esemeny: esemeny1Value, // the new element's value here becometh the called variable
             evszam: evszam1Value // the new element's value here becometh the called variable
         }   
         array.push(newElement); // we push this to the array
-        if (esemeny2Value != undefined || evszam2Value != undefined) {
+        if (esemeny2Value != undefined || evszam2Value != undefined) { // }
             const newElement2 = { // we make new element
                 esemeny: esemeny2Value, // the new element's value here becometh the called variable
                 evszam: evszam2Value // the new element's value here becometh the called variable
             }
             array.push(newElement2); // we push this to the array
         }
-        tbody.innerHTML = ''; // we delete the contents of the previous tbodeehhh
+        table.innerHTML = ''; // we delete the contents of the previous tbodeehhh
         renderMenu(); // this re-renders the table
         thisForm.reset(); // this removes everything from the form
     }
-    
 })
